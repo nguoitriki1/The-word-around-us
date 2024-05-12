ed7cc8958884d0d54cac246afbf9def2d1a6731e
IUI <congdoc19101990@gmail.com>
2024-05-12 15:21:51 +0700

    save

--- app/src/main/java/com/example/theworldaroundus/splash/SplashActivity.kt
+++ app/src/main/java/com/example/theworldaroundus/splash/SplashActivity.kt
@@ -6,16 +6,30 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import coil.compose.rememberAsyncImagePainter
+import com.example.theworldaroundus.R
 import com.example.theworldaroundus.main.MainActivity
 import com.example.theworldaroundus.ui.theme.TheWorldAroundUsTheme
+import com.example.theworldaroundus.ui.theme.Typography
+import com.example.theworldaroundus.ui.theme.WhiteColor
 import kotlinx.coroutines.delay
 
 @SuppressLint("CustomSplashScreen")
@@ -33,20 +47,30 @@ class SplashActivity : ComponentActivity() {
                         finish()
                     }
 
-                    SplashContent(
-                        name = "Welcome to the World Around Us",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    Box(modifier = Modifier.fillMaxSize()) {
+                        Image(
+                            painter = rememberAsyncImagePainter(model = R.drawable.background_splash),
+                            contentDescription = "",
+                            contentScale = ContentScale.FillBounds
+                        )
+
+                        Text(
+                            modifier = Modifier
+                                .padding(bottom = 16.dp)
+                                .padding(horizontal = 16.dp)
+                                .padding(innerPadding)
+                                .align(Alignment.BottomCenter),
+                            text = stringResource(R.string.welcome_to_the_world_around_us)+"!",
+                            textAlign = TextAlign.Center,
+                            style = Typography.titleMedium.copy(
+                                color = WhiteColor,
+                                fontSize = 16.sp,
+                                fontWeight = FontWeight.Medium
+                            )
+                        )
+                    }
                 }
             }
         }
     }
-}
-
-@Composable
-fun SplashContent(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "$name!",
-        modifier = modifier
-    )
 }
